6a957c4348ae67f6173c333de6fcdefc61b77f3b
Shimaa Adel <eng.sh.adel@gmail.com>
2022-07-01 23:50:32 +0200

    add middleware for logging , security

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,7 @@
 import express, { Application, Request, Response } from 'express'
 import morgan from 'morgan'
+import helmet from 'helmet'
+import RateLimit from 'express-rate-limit'
 import * as dotenv from 'dotenv'
 
 dotenv.config()
@@ -7,9 +9,23 @@ dotenv.config()
 const PORT = process.env.PORT || 3000
 // create an instance server
 const app: Application = express()
+//middleware to parse incoming requests
+app.use(express.json())
 // HTTP request logger middleware
 app.use(morgan('short'))
+// securing middleware
+app.use(helmet())
 
+//apply rate limit
+app.use(
+  RateLimit({
+    windowMs: 15 * 60 * 1000, // 15 minutes
+    max: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
+    standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
+    legacyHeaders: false, // Disable the `X-RateLimit-*` headers'
+    message: 'Too many requests, please try again in 15 mins '
+  })
+)
 // add routing for / path
 app.get('/', (req: Request, res: Response) => {
   res.json({
